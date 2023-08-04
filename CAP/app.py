0867c7dfc8f654c5ea9bda83ee4887590c006d7c
SriG2000 <137358034+SriG2000@users.noreply.github.com>
2023-08-04 14:40:44 -0700

    Dashboard Works

--- CAP/app.py
+++ CAP/app.py
@@ -105,8 +105,8 @@ def dashboard():
             if result:
                 user_name = result[0]
 
-            # Fetch all posts from the user_posts table (or any other table as per your requirement)
-            select_query = "SELECT name, DAT, topic, post FROM user_posts"
+            # Fetch all posts from the user_posts table (including the "company" information)
+            select_query = "SELECT name, DAT, topic, post, company, industry FROM user_posts"
             cursor.execute(select_query)
             posts = cursor.fetchall()
 
@@ -122,5 +122,149 @@ def dashboard():
     # If the user is not logged in, redirect to the login page
     return redirect(url_for('login'))
 
+@app.route('/create_post')
+def create_post():
+    return render_template('createpost.html')
+
+@app.route('/save_post', methods=['POST'])
+def save_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        company_name = request.form['company_name']
+        industry = request.form['industry']
+        post_content = request.form['post_content']
+        
+        # Retrieve the user's name from the session
+        if 'email' in session:
+            email = session['email']
+            user_name = None
+            
+            try:
+                connection = mysql.connector.connect(**db_config)
+                cursor = connection.cursor()
+                
+                # Get the user's name from the users table
+                select_query = "SELECT name FROM users WHERE email = %s"
+                cursor.execute(select_query, (email,))
+                result = cursor.fetchone()
+                
+                if result:
+                    user_name = result[0]
+                    
+                    # Insert the post into the user_posts table
+                    insert_query = "INSERT INTO user_posts (name, DAT, topic, post, industry, company) VALUES (%s, NOW(), %s, %s, %s, %s)"
+                    data = (user_name, title, post_content, industry, company_name)
+                    cursor.execute(insert_query, data)
+                    connection.commit()
+                    
+                    cursor.close()
+                    connection.close()
+                    
+                    # Redirect to the dashboard page after saving the post
+                    return redirect(url_for('dashboard'))
+                
+            except mysql.connector.Error as error:
+                print("Error while connecting to MySQL:", error)
+
+    return redirect(url_for('create_post'))
+
+@app.route('/edit_profile')
+def edit_profile():
+    # Retrieve the user's name and other details from the database (replace this with your actual code)
+    if 'email' in session:
+        email = session['email']
+        user_data = None
+
+        try:
+            connection = mysql.connector.connect(**db_config)
+            cursor = connection.cursor()
+
+            # Get the user's details from the users table
+            select_query = "SELECT name, email, industry FROM users WHERE email = %s"
+            cursor.execute(select_query, (email,))
+            result = cursor.fetchone()
+
+            if result:
+                user_data = {
+                    'name': result[0],
+                    'email': result[1],
+                    'industry': result[2]
+                }
+
+            cursor.close()
+            connection.close()
+
+            # Render the editprofile.html template and pass the user's details to it
+            return render_template('editprofile.html', user=user_data)
+
+        except mysql.connector.Error as error:
+            print("Error while connecting to MySQL:", error)
+
+    # If the user is not logged in, redirect to the login page
+    return redirect(url_for('login'))
+
+@app.route('/save_changes', methods=['POST'])
+def save_changes():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        industry = request.form['industry']
+
+        # Update the user's details in the database
+        if 'email' in session:
+            try:
+                connection = mysql.connector.connect(**db_config)
+                cursor = connection.cursor()
+
+                update_query = "UPDATE users SET name = %s, email = %s, password = %s, industry = %s WHERE email = %s"
+                data = (name, email, password, industry, session['email'])
+                cursor.execute(update_query, data)
+                connection.commit()
+
+                cursor.close()
+                connection.close()
+
+            except mysql.connector.Error as error:
+                print("Error while connecting to MySQL:", error)
+
+            # Redirect to the dashboard page after saving the changes
+            return redirect(url_for('dashboard'))
+
+    return redirect(url_for('edit_profile'))
+
+@app.route('/flag_post', methods=['POST'])
+def flag_post():
+    if request.method == 'POST':
+        post_id = request.form['post_id']
+
+        try:
+            connection = mysql.connector.connect(**db_config)
+            cursor = connection.cursor()
+
+            # Update the 'flag' column to 1 (flagged) for the post with the given post_id
+            update_query = "UPDATE user_posts SET flag = %s WHERE id = %s"
+            data = (1, post_id)
+            cursor.execute(update_query, data)
+            connection.commit()
+
+            cursor.close()
+            connection.close()
+
+            # Redirect back to the dashboard after flagging the post
+            return redirect(url_for('dashboard'))
+
+        except mysql.connector.Error as error:
+            print("Error while connecting to MySQL:", error)
+
+    # If there is an error or the method is not POST, redirect back to the dashboard
+    return redirect(url_for('dashboard'))
+
+@app.route('/logout')
+def logout():
+    # Clear the session to log out the user
+    session.clear()
+    # Redirect to the login page after logging out
+    return redirect(url_for('login'))
 if __name__ == '__main__':
     app.run(debug=True)
